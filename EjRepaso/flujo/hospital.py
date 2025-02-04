d448e05aab55361cb933c5f6bc63de6776d4ef67
mateozorzi <mzorzi@fi.uba.ar>
2025-02-04 17:36:47 -0300

    ej hospi

--- EjRepaso/flujo/hospital.py
+++ EjRepaso/flujo/hospital.py
@@ -10,23 +10,32 @@ def hospital(pacientes, medicos):
         g.agregar_vertice(p[0])
         g.agregar_arista("Fuente", p[0], 1)
     
-    especialidades = set()
-    horarios = set()
+    especialidades = {}
     for m in medicos:
-        if m[1] not in especialidades:
-            g.agregar_vertice(m[1])
-            especialidades.add(m[1])
+        g.agregar_vertice(m[0])
+        g.agregar_arista(m[0], "Sumidero", len(m[2]))
+        especialidad = m[1]
+        horarios = m[2]
+
+        if especialidad not in especialidades:
+            especialidades[especialidad] = set()
         
-        for h in m[2]:
-            if h not in horarios:
-                g.agregar_vertice(m[2])
-                g.agregar_arista(m[1], h,1)
-                g.agregar_arista(m[2], "Sumidero", "inf")
-                horarios.add(m[2])
-    
+        for h in horarios:
+            if h not in especialidades[especialidad]:
+                especialidades[especialidad].add(h)
+                g.agregar_vertice(especialidad + "-" + h)
+                g.agregar_arista(especialidad + "-" + h, m[0], 1)
+
+        
     for p in pacientes:
+        especialidad = p[1]
+        horarios = p[2]
         for e in especialidades:
-            g.agregar_arista(p[0], e, 1)
+            if e == especialidad:
+                for h in horarios:
+                    if h in especialidades[e]:
+                        g.agregar_arista(p[0], e + "-" + h, 1)
+                break
 
     return g
 #Flujo -> cantidad de turnos que se asignan a los medicos del hospital
